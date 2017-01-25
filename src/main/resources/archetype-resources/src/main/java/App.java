c37ea5d5205c6a5cfb0deae509274ee2291452ee
Anthony <anthony.kulis@gmail.com>
2017-01-25 14:40:41 -0600

    adds in consumer/producer for testing

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,13 +1,28 @@
 package com.saasforge;
 
+import org.apache.camel.main.Main;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App{
+    public static void main(String... args){
+
+        System.out.println("************************************");
+        System.out.println("* Press ctrl+c to stop the example *");
+        System.out.println("************************************");
+
+        Main main = new Main();
+
+        ConsumerRoute consumerRoute = new ConsumerRoute();
+        ProducerRoute producerRoute = new ProducerRoute();
+
+        main.addRouteBuilder(consumerRoute);
+        main.addRouteBuilder(producerRoute);
+
+        main.run();
+
+
     }
 }
