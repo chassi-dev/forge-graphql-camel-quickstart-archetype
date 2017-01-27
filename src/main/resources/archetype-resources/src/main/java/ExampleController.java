939de198d980c6ef4bcbfacd696e575329bf3e48
Anthony <anthony.kulis@gmail.com>
2017-01-27 10:00:48 -0600

    init but getting it to work with java and not groovy

--- src/main/resources/archetype-resources/src/main/java/ExampleController.java
+++ src/main/resources/archetype-resources/src/main/java/ExampleController.java
@@ -3,12 +3,19 @@ package ${package};
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class ExampleController {
 
-	@RequestMapping(value = "/")
-	public String home() {
-		return "Request sent: " + new ExampleProducer().sendMessage();
+	@RequestMapping(value = "/graphql", method = RequestMethod.GET)
+	public void graphQLQuery(@RequestParam("query") String query) {
+//		Graphql executed = new GraphQL(Query.querySchema).execute(query)
+//						["data": executed.data, "errors": executed.errors] as Map
 	}
 
+	@RequestMapping(value = "/graphql")
+	public void graphQLMutation(@RequestBody String body){
+//		def executed = new GraphQL(Query.querySchema).execute(body)
+//						["data": executed.data, "errors": executed.errors] as Map
+	}
 }
